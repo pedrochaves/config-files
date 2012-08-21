41bc3a8ad826c9ddb0f93b4cc953ad5e1ec0dbec
Pedro Chaves <pcohc3m@gmail.com>
2012-08-21 09:27:16 -0300

    Adding pyvows dependencies

--- scripts/02-config-python.sh
+++ scripts/02-config-python.sh
@@ -4,10 +4,10 @@
 # Author: Pedro Chaves
 # Since: 18/08/2012
 
-# Installing pip
-apt-get install -y python-pip && \
+# Installing pip and pyvows dependencies
+apt-get install -y python-pip libxslt-dev libxml2-dev && \
 # Dependencies for python MySQLdb lib
 apt-get build-dep python-mysqldb && \
-# Python libs =]
+# And finally, the libs :)
 pip install -r python-requirements.txt
 
