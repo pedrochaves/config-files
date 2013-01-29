7b75ab8945a354fdcaccf8769ceeb843fadf6d24
Pedro Chaves <pcohc3m@gmail.com>
2013-01-28 22:46:04 -0200

    Adding Google Chrome to the env script

--- bash-scripts/01-config-env.sh
+++ bash-scripts/01-config-env.sh
@@ -4,5 +4,7 @@
 # Author: Pedro Chaves
 # Since: 18/08/2012
 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update && \
-apt-get install -y vim git g++ libmysqlclient-dev mysql-server mysql-client mysql-workbench nginx
+apt-get install -y google-chrome-stable vim git g++ libmysqlclient-dev mysql-server mysql-client mysql-workbench nginx
